e7f67a35442b4c8f0ff8a04c9d7f0abd166e7ab2
Projects <nawalgokul4@gmail.com>
2026-09-23 17:31:12 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Localift/',   // ← add this line
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/Localift/',
 })
